6e665df356a6745c0a84425e14b3219cd264e731
ilya <ilya@indeed.com>
2017-07-30 10:45:01 +0900

    Initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlalgos',
+    version='0.1',
+    description='Basic ML algorithms',
+    url='https://github.com/machinehead/mlalgos.git',
+    author='Ilya Nekhay',
+    author_email='nekhayiv@gmail.com',
+    license='MIT',
+    packages=['mlalgos'],
+    zip_safe=False
+)
